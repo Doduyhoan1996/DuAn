2dc9b56de0db70fd5197aa36555c0fa7cffaca8b
Admin <Admin>
2019-05-21 19:53:05 +0700

    DuAn

--- index.php
+++ index.php
@@ -7,11 +7,37 @@
 </head>
 <body>
 <?php
+$height = $weight=$pl=$benh='';
 if (isset($_POST['height']) && isset($_POST['weight'])){
-    $height = (int) $_POST['height'];
-    $weight = (int) $_POST['weight'];
+    $height = (float) $_POST['height'];
+    $weight = (float) $_POST['weight'];
     $bmi =$weight/($height*$height);
+
+    if($bmi<18.5){
+        $pl="Gầy";
+        $benh="Thấp";
+    }elseif (18.5 <=$bmi && $bmi <= 24.9){
+        $pl="Bình Thường";
+        $benh="TB";
+    }elseif (25 <=$bmi && $bmi <= 29.9){
+        $pl="Hơi Béo";
+        $benh="Cao";
+    }elseif (30 <= $bmi && $bmi <= 34.9){
+        $pl="Béo Cấp độ 1";
+        $benh="Cao";
+    }elseif (35 <= $bmi && $bmi <= 39.9) {
+        $pl = "Béo Cấp độ 2";
+        $benh = "Rất Cao";
+    }elseif ( $bmi > 40.9) {
+        $pl = "Béo Cấp độ 3";
+        $benh = "Nguy hiểm";
+    }
+
     echo "chỉ số BMI là $bmi";
+    echo "<br>";
+    echo "phân loại: $pl";
+    echo "<br>";
+    echo "nguy cơ phát triển bệnh: $benh";
 }
 ?>
 <div class="container">
@@ -20,11 +46,11 @@ if (isset($_POST['height']) && isset($_POST['weight'])){
         <form name="bmi" action="" method="post">
             <div class="form-group">
                 <label>Chiều Cao (m)</label>
-                <input type="text" class="form-control" name="height" placeholder="Nhập chiều cao (m)" required>
+                <input type="text" class="form-control" name="height" placeholder="Nhập chiều cao (m)" value="<?php echo $height ?>" required>
             </div>
             <div class="form-group">
                 <label>Cân nặng (kg)</label>
-                <input type="text" class="form-control" name="weight" placeholder="Nhập cân nặng (kg)" required>
+                <input type="text" class="form-control" name="weight" placeholder="Nhập cân nặng (kg)" value="<?php echo $weight ?>" required>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
