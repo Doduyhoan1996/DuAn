9e093e2ed513dc29255d3c3eb0dfee8cacd4353f
Admin <Admin>
2019-05-21 19:25:15 +0700

    DuAn

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 21/05/2019
- * Time: 18:37 CH
- */
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+<h1>Tính chỉ số BMI</h1>
+<form name="bmi" action="" method="post">
+    <div class="form-group">
+        <label>Chiều Cao</label>
+        <input type="text" class="form-control" name="height" placeholder="Nhập chiều cao" required>
+    </div>
+    <div class="form-group">
+        <label>Chiều Cao</label>
+        <input type="text" class="form-control" name="weight" placeholder="Nhập cân nặng" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</body>
+</html>
